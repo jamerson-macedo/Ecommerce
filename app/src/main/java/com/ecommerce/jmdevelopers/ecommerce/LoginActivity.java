b722a3ed62e36546a74f3077592e9ce2a550273e
jamerson_macedo@hotmail.com <jamerson_macedo@hotmail.com>
2018-11-09 01:16:00 -0200

    animações na tela de login

--- app/src/main/java/com/ecommerce/jmdevelopers/ecommerce/LoginActivity.java
+++ app/src/main/java/com/ecommerce/jmdevelopers/ecommerce/LoginActivity.java
@@ -2,12 +2,24 @@ package com.ecommerce.jmdevelopers.ecommerce;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private ImageView logo;
+    private Button botaologin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        logo=findViewById(R.id.login_logo);
+        botaologin=findViewById(R.id.btt_login_activity);
+        Animation fadein=AnimationUtils.loadAnimation(this,R.anim.fade_in);
+        Animation subir=AnimationUtils.loadAnimation(this,R.anim.subir);
+        logo.startAnimation(fadein);
+        botaologin.startAnimation(subir);
+
     }
 }
